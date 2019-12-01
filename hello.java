eda4d24b08e2ffe586af6f7ad2c1f3e13eee5298
Iris Foox <57198497+fooxit1239@users.noreply.github.com>
2019-12-01 14:16:46 +0200

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,17 @@
+package hello;
+
+import javax.swing.JOptionPane;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        // the simplest java there is:
+	    System.out.println("hello world");
+	    //and if we want to step it up a bit:
+	    String name=JOptionPane.showInputDialog("enter your name");
+	    JOptionPane.showMessageDialog(null, "hello world\nand hello "+name+"\nhow are you today?");;
+	    
+	}
+
+}
