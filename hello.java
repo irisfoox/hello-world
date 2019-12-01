92c09b195d961e25e4510b03e4bdc1c134d577fd
Iris Foox <57198497+fooxit1239@users.noreply.github.com>
2019-12-01 14:18:58 +0200

    Update hello.java

--- hello.java
+++ hello.java
@@ -6,12 +6,10 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        // the simplest java there is:
+            // the simplest java there is:
 	    System.out.println("hello world");
 	    //and if we want to step it up a bit:
 	    String name=JOptionPane.showInputDialog("enter your name");
-	    JOptionPane.showMessageDialog(null, "hello world\nand hello "+name+"\nhow are you today?");;
-	    
+	    JOptionPane.showMessageDialog(null, "hello world\nand hello "+name+"\nhow are you today?");;  
 	}
-
 }
